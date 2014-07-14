80a2e44eac91e7c541150fc4e3a6be1f0f95bf08
Lars Greiss <kufikugel@googlemail.com>
2014-07-14 02:16:38 +0200

    bacon: inherit the common slim/google way
    no need to do here extra stuff...do it like all other devices
    
    as well remove the vendor setup entries....we do it central
    
    Change-Id: I451e0c66aa8fad6b6ddc4c170a38d5e5e6dfe097

--- slim.mk
+++ slim.mk
@@ -15,7 +15,7 @@ $(call inherit-product, vendor/slim/config/common_full_phone.mk)
 $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/oneplus/bacon/bacon.mk)
+$(call inherit-product, device/oneplus/bacon/full_bacon.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := bacon
