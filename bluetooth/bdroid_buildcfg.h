ccb92ca4f7845c0d1454357f58c28c8e5a170121
Lars Greiss <kufikugel@googlemail.com>
2014-07-19 18:25:49 +0200

    bacon: bringup for slim BT stack
    Change-Id: Ibd8c200a0257c1ded61d691d4339d0ef6cde8e56

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,14 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BLUETOOTH_QCOM_SW TRUE
-
 #define BTM_DEF_LOCAL_NAME   "OnePlus One"
-// Disables read remote device feature
-#define BTA_SKIP_BLE_READ_REMOTE_FEAT TRUE
-#define MAX_L2CAP_CHANNELS    14
-// skips conn update at conn completion
-#define BTA_BLE_SKIP_CONN_UPD  TRUE
-// Enables interleaved scan
-#define BTA_HOST_INTERLEAVE_SEARCH TRUE
+#define BTA_DISABLE_DELAY 1000 /* in milliseconds */
+
 #endif
