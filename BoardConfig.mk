f18220bb8d3e3ff240d00e2ddc215cb69dbe3b17
RobbieL811 <RobbieLancaster811@gmail.com>
2014-10-08 19:45:43 -0400

    Switch to linaro-4.8-14.04 toolchain
    Change-Id: I41b45d1213882313e81de71230f92d4839322f9e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,7 @@
 TARGET_KERNEL_CONFIG := slim_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3
 BOARD_CUSTOM_BOOTIMG_MK := device/oneplus/bacon/mkbootimg.mk
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.7-13.04/bin/arm-linux-gnueabihf-
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.8-14.04/bin/arm-linux-gnueabihf-
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
