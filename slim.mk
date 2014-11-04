f6df541dad1054491d1cb527024d66c9d7f93eec
RobbieL811 <RobbieLancaster811@gmail.com>
2014-11-03 23:09:13 -0500

    Fix device ID
    Change-Id: I62d44a7df939aba602c897e19c8cba66426231cb
    Signed-off-by: RobbieL811 <RobbieLancaster811@gmail.com>

--- slim.mk
+++ slim.mk
@@ -18,11 +18,17 @@ $(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 $(call inherit-product, device/oneplus/bacon/full_bacon.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := bacon
 PRODUCT_NAME := slim_bacon
-PRODUCT_BRAND := oneplus
-PRODUCT_MODEL := One A0001
+PRODUCT_DEVICE := bacon
 PRODUCT_MANUFACTURER := OnePlus
+PRODUCT_MODEL := A0001
+
+PRODUCT_GMS_CLIENTID_BASE := android-oneplus
+
+PRODUCT_BRAND := oneplus
+TARGET_VENDOR_PRODUCT_NAME := bacon
+TARGET_VENDOR_DEVICE_NAME := A0001
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_DEVICE=A0001 PRODUCT_NAME=bacon
 
 TARGET_CONTINUOUS_SPLASH_ENABLED := true
 
