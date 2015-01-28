d581c733151f05acdcade5b56fa396074a8d2bc9
RobbieL811 <RobbieLancaster811@gmail.com>
2015-01-28 11:31:06 -0500

    Switch to new custom kernel toolchain flag
    Change-Id: I72d81f440470c3b270a17b30cdddae1f1b2a5965
    Signed-off-by: RobbieL811 <RobbieLancaster811@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,8 @@
 TARGET_KERNEL_CONFIG := slim_bacon_defconfig
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=bacon user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 androidboot.bootdevice=msm_sdcc.1
 BOARD_CUSTOM_BOOTIMG_MK := device/oneplus/bacon/mkbootimg.mk
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro-4.9-14.11/bin/arm-linux-gnueabihf-
+KERNEL_TOOLCHAIN_PREFIX := arm-linux-gnueabihf-
+KERNEL_TOOLCHAIN := "$(ANDROID_BUILD_TOP)/prebuilt/$(HOST_OS)-x86/toolchain/linaro-4.9-14.06/bin/"
 
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/oneplus/bacon/bluetooth
