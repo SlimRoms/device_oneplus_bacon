311c7d441588814091408ca635c73f3a2e5dba5a
Sultanxda <sultanxda@gmail.com>
2016-03-12 21:41:28 -0800

    bacon: Add back missing NFC parts
    These things were removed from Nexus device trees, but they are still relevant
    to Bacon.

--- bacon.mk
+++ bacon.mk
@@ -150,7 +150,16 @@ PRODUCT_PACKAGES += \
     libxml2
 
 # NFC
+ifeq ($(TARGET_BUILD_VARIANT),user)
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access.xml
+else
+    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/configs/nfcee_access_debug.xml
+endif
+PRODUCT_COPY_FILES += \
+    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+
 PRODUCT_PACKAGES += \
+    com.android.nfc_extras \
     NfcNci \
     nfc_nci.pn54x.default \
     Tag
