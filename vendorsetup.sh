86f24c1648e078412802f9d649c2f9e952cf6fdb
Dan Cartier <nospamdan1@gmail.com>
2016-08-06 16:49:01 +0200

    bacon: Update vendorsetup
    Change-Id: I1cf3be9a67ba85b96308d8f1f8ae4e3bcae2b84b

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
-add_lunch_combo cm_bacon-user
-add_lunch_combo cm_bacon-userdebug
-add_lunch_combo cm_bacon-eng
+add_lunch_combo slim_bacon-user
+add_lunch_combo slim_bacon-userdebug
+add_lunch_combo slim_bacon-eng
