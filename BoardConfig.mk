f26c69147101013035e47c58403c59137ba4c5eb
Dan Cartier <nospamdan1@gmail.com>
2016-09-11 03:44:29 +0200

    bacon: Remove dex preopt for now...
    Change-Id: I79be49eb261381d72faa7b02be2ca9ca5480596a

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,15 +89,6 @@ BOARD_USES_CYANOGEN_HARDWARE := true
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-  ifeq ($(call match-word-in-list,$(TARGET_BUILD_VARIANT),user),true)
-    ifeq ($(WITH_DEXPREOPT_BOOT_IMG_ONLY),)
-      WITH_DEXPREOPT_BOOT_IMG_ONLY := true
-    endif
-  endif
-endif
-
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
