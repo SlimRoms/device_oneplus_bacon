ef141b80e077dc404e4bfb50f2d2c9317e595835
Dan Cartier <nospamdan1@gmail.com>
2016-09-11 08:50:31 +0200

    bacon: Disable sepolicy for now
    Change-Id: I00c0c2dfa441a79818637de898fe10b08bd19c73

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -160,10 +160,10 @@ TARGET_RECOVERY_FSTAB := $(PLATFORM_PATH)/rootdir/etc/fstab.bacon
 TARGET_NO_RPC := true
 
 # SELinux
-include device/qcom/sepolicy/sepolicy.mk
+#include device/qcom/sepolicy/sepolicy.mk
 
-BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
+#BOARD_SEPOLICY_DIRS += \
+#    $(PLATFORM_PATH)/sepolicy
 
 # Wifi
 BOARD_HAS_QCOM_WLAN              := true
