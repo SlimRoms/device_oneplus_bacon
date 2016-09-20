ff5831c13f74a7ce65cffb3f52af888911205349
Dan Cartier <nospamdan1@gmail.com>
2016-09-20 06:37:49 -0400

    bacon: Build with GCC LIBC for Sensors
    Sets TARGET_NEEDS_GCC_LIBC := true in board config.
    
    Needs https://review.slimroms.org/#/c/20023/
    
    Change-Id: Ice01fff3c5a0abd0b237353b7c272c48779ffb25

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,8 +164,9 @@ TARGET_NO_RPC := true
 
 # Sensors
 BOARD_GLOBAL_CFLAGS += -DCOMPAT_SENSORS_M
+TARGET_NEEDS_GCC_LIBC := true
 
-# Legacy Blobs
+# Text relocations for legacy blobs
 TARGET_NEEDS_TEXT_RELOCATIONS := true
 
 # SELinux
