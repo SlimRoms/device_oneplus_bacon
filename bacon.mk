dd6b3e36266e0235ac7470dd26a26a30bc448ffc
Dan Cartier <nospamdan1@gmail.com>
2016-10-07 10:57:18 -0400

    bacon: Disable the camera server by specifying HAL1
    * Needs these two flags in device tree:
      TARGET_HAS_LEGACY_CAMERA_HAL1 := true
      media.stagefright.legacyencoder=true
    
    * See http://review.cyanogenmod.org/#/c/165009/2 for details
    
    Change-Id: Ifadc434b2b9a593f3ca50aa484c28df12f317493

--- bacon.mk
+++ bacon.mk
@@ -73,7 +73,8 @@ PRODUCT_PACKAGES += \
     camera.bacon
 
 PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.codecremote=false
+    media.stagefright.codecremote=false \
+    media.stagefright.legacyencoder=true
 
 # Charger
 PRODUCT_PACKAGES += \
