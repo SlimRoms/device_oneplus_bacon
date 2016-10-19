e2764b156a0f036d0b9c88a51d7e4f61c2260ea4
Dan Cartier <nospamdan1@gmail.com>
2016-10-19 15:44:20 -0400

    bacon: Move stagefright props to system.prop
    * Add less-secure flag
    * Remove codecremote
    
    Change-Id: I5a789346aca3c9472c9b212679b12c5571622b37

--- bacon.mk
+++ bacon.mk
@@ -68,9 +68,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     camera.bacon
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    media.stagefright.codecremote=false
-
 # Charger
 PRODUCT_PACKAGES += \
     charger_res_images
